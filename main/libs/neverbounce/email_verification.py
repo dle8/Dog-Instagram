c52a16c66fba373baf494f4b4d26440ca6f9206f
dle8 <dle8@u.rochester.edu>
2019-09-07 11:35:42 -0400

    Libs directory safely uses third party API by wrapping API calls in try except

---
+++ main/libs/neverbounce/email_verification.py
@@ -0,0 +1,17 @@
+import requests
+from main.config import config
+import json
+
+NEVERBOUNCE_EMAIL_VERFICATION_URL = 'https://api.neverbounce.com/v4/single/check?key={}&email={}'
+
+
+def validate_email(email):
+    try:
+        api_response = requests.post(
+            NEVERBOUNCE_EMAIL_VERFICATION_URL.format(config.NEVERBOUNCE_API_KEY, email)
+        ).content
+        api_response = json.loads(api_response)
+    except Exception:
+        raise Exception('There is an error connnecting to NeverBounce server.')
+    if api_response['result'] == 'invalid':
+        raise errors.InvalidEmail
