4152555418aedb9e47fa73ebd736f689079e5933
dle8 <dle8@u.rochester.edu>
2019-09-07 10:54:57 -0400

    Init app and its extensions

---
+++ main/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_mail import Mail
+from flask_cors import CORS
+from celery import Celery
+from main.config import config
+
+app = Flask(__name__, template_folder='../templates')
+app.config.from_object(config)
+
+
+def _register_subpackages():
+    import main.models
+    import main.errors
+    import main.controllers
+
+
+# Initialize Celery
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+# Initializes the application with the following extensions.
+mail = Mail()
+mail.init_app(app)
+
+_register_subpackages()
+
+cors = CORS()
+cors.init_app(app)
