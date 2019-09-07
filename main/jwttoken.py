b2fc60228e0429a1dd44ce2e278187b55f3743e8
dle8 <dle8@u.rochester.edu>
2019-09-07 16:22:24 -0400

    Add algorithm to decode jwt token

--- main/jwttoken.py
+++ main/jwttoken.py
@@ -7,13 +7,13 @@ from main.config import config
 def encode(account):
     iat = datetime.datetime.utcnow()
     return jwt.encode({
-        'sub': account.email,
+        'sub': account['email'],
         'iat': iat,
         'exp': iat + datetime.timedelta(days=365),
-    }, config.JWT_SECRET)
+    }, config.JWT_SECRET, algorithm=config.JWT_ALGORITHM)
 
 
-def decode(access_token, audience):
+def decode(access_token):
     try:
         payload = jwt.decode(access_token, config.JWT_SECRET)
     except jwt.InvalidTokenError:
