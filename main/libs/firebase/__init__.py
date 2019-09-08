5c78c342e64dd56cdce928f02d1c6fd9324913c0
dle8 <dle8@u.rochester.edu>
2019-09-07 20:51:06 -0400

    Initialize firebase database and storage

---
+++ main/libs/firebase/__init__.py
@@ -0,0 +1,13 @@
+import firebase_admin
+from firebase_admin import credentials, db, storage
+from main.config import config
+
+cred = credentials.Certificate(config.CREDENTIALS_DIR_PATH + '/' + config.FIREBASE_ADMINSDK_JSON)
+firebase_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': config.DATABASE_URL,
+    'storageBucket': config.STORAGE_BUCKET
+})
+
+root = db.reference()
+users_ref = root.child('users')
+bucket = storage.bucket()
