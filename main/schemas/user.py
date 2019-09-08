8ad4ae42c0df64a3859649b2c4d34a1944546189
dle8 <dle8@u.rochester.edu>
2019-09-08 19:36:45 -0400

    Add validating email for schemas

--- main/schemas/user.py
+++ main/schemas/user.py
@@ -1,12 +1,24 @@
-from marshmallow import fields, validate
+from marshmallow import fields, validate, validates
 from main.schemas.base import BaseSchema
+from main.libs.neverbounce.email_verification import validate_email
 
 
 class AuthSchema(BaseSchema):
     email = fields.Email(required=True)
     password = fields.String(required=True, validate=validate.Length(min=6))
 
+    @validates('email')
+    def validate_email(self, email):
+        if email:
+            validate_email(email)
+
 
 class ConfirmationCodeSchema(BaseSchema):
     confirmation_code = fields.Integer(required=True)
     email = fields.Email(required=True)
+
+    @validates('email')
+    def validate_email(self, email):
+        if email:
+            validate_email(email)
+
