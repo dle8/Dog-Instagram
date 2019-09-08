163dac6313619902a344a6ec416bb53f6c3c75f2
dle8 <dle8@u.rochester.edu>
2019-09-07 23:49:07 -0400

    Modify methods' names

--- main/libs/firebase/image.py
+++ main/libs/firebase/image.py
@@ -4,44 +4,57 @@ from werkzeug.utils import secure_filename
 import datetime
 
 
-def upload_image(file=None, email=None):
-    blob = bucket.blob(email + '/' + secure_filename(file.filename))
-    blob.upload_from_file(file)
+def upload_user_image(image=None, email=None):
+    try:
+        blob = bucket.blob(email + '/' + secure_filename(image.filename))
+        blob.upload_from_file(image)
+    except Exception:
+        raise Exception('There is an error uploading user picture')
 
 
-def get_images(email=None):
-    blobs = bucket.list_blobs(prefix=email)
-    images = [{
-        'url': blob.generate_signed_url(datetime.timedelta(seconds=3600), method='GET'),
-        'name': blob.name
-    } for blob in blobs]
+def get_user_images(email=None):
+    try:
+        blobs = bucket.list_blobs(prefix=email)
+        images = [{
+            'url': blob.generate_signed_url(datetime.timedelta(seconds=3600), method='GET'),
+            'name': blob.name
+        } for blob in blobs]
 
-    return images
-
-
-def delete_image(file=None, email=None):
-    blob = bucket.blob(email + '/' + file)
-    blob.delete()
+        return images
+    except Exception:
+        raise Exception('There is an error getting user picture')
 
 
-def get_followees_images(email=None):
-    images = []
-    user_images = get_images(email)
-    for user_image in user_images:
-        images.append({
-            'url': user_image['url'],
-            'name': user_image['name'],
-            'email': email
-        })
-    user_key = get_user_key_or_none(email=email)
-    followees = users_ref.child(user_key).child('followees').get()
-    for key, value in followees:
-        followee_images = get_images(value['email'])
-        for followee_image in followee_images:
+def delete_user_image(image_name=None, email=None):
+    try:
+        blob = bucket.blob(email + '/' + image_name)
+        blob.delete()
+    except Exception:
+        raise Exception('There is an error deleting user picture')
+
+
+def get_user_followees_images(email=None):
+    try:
+        images = []
+        user_images = get_user_images(email)
+        for user_image in user_images:
             images.append({
-                'url': followee_image['url'],
-                'name': followee_image['name'],
-                'email': value['email']
+                'url': user_image['url'],
+                'name': user_image['name'],
+                'email': email
             })
 
-    return images
+        user_key = get_user_key_or_none(email=email)
+        followees = users_ref.child(user_key).child('followees').get()
+        for key, value in followees:
+            followee_images = get_user_images(value['email'])
+            for followee_image in followee_images:
+                images.append({
+                    'url': followee_image['url'],
+                    'name': followee_image['name'],
+                    'email': value['email']
+                })
+
+        return images
+    except Exception:
+        raise Exception('There is an error getting followees\' pictures')
